3dd14b37c282f5e9896dfd53783ba450be5bf260
Liubou <82825264+Liubou888@users.noreply.github.com>
2021-06-21 00:14:21 +0300

    complete task

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,55 @@
 package subtask3
 
+import java.lang.Math.abs
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        //  return when(blockB.javaObjectType){
+        return when(blockB){
+            String::class -> concatenation(blockA)
+            Int::class -> sum(blockA)
+            LocalDate::class -> latestDate(blockA)
+            else -> throw IllegalArgumentException("Illegal class in blockB: ${blockB.javaObjectType.name}")
+        }
     }
+
+
+    private fun concatenation(blockA: Array<*>): Any {
+        var result = ""
+        for (element in blockA){
+            if (element is String){
+                result += element
+            }
+        }
+        return result
+    }
+
+    private fun sum(blockA: Array<*>): Any {
+        var sum = 0
+        for (element in blockA){
+            if (element is Int){
+                sum += element
+            }
+        }
+        return sum
+    }
+
+
+    private fun latestDate(blockA: Array<*>): Any {
+        var tempDate = LocalDate.MIN
+        for (element in blockA){
+            if (element is LocalDate && tempDate.isBefore(element)){
+                tempDate = element
+            }
+        }
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        return formatter.format(tempDate)
+    }
+
+
 }
+
+
