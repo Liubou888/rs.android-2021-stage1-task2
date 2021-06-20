12fc6cd74641cd5c0c9127f89913bc277652621d
Liubou <82825264+Liubou888@users.noreply.github.com>
2021-06-21 00:13:40 +0300

    update subtusk2

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,20 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var aStr =a.toUpperCase()
+        val bUpperCase = b.toCharArray()
+        bUpperCase.forEachIndexed{index, char -> bUpperCase[index]=char.toUpperCase()}
+        for(charOfB in bUpperCase){
+            if (aStr.indexOf(charOfB)<0)return "NO" else
+                aStr = aStr.substring(aStr.indexOf(charOfB)+1,aStr.length)
+        }
+        return "YES"
     }
+
 }
+
+fun main(){
+    println(Abbreviation().abbreviationFromA("ytrewq","RY"))
+}
+
+
